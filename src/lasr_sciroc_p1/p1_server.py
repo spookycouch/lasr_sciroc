942b21134474612ecdd2d74d010ed1896325c34d
Mohammed Alshammasi <fy16m3aa@leeds.ac.uk>
2019-08-06 01:21:33 +0000

    Added parameter server usage for storage

--- src/lasr_sciroc_p1/p1_server.py
+++ src/lasr_sciroc_p1/p1_server.py
@@ -35,32 +35,17 @@ class P1Server(object):
         self.play_motion_client = actionlib.SimpleActionClient('/play_motion', PlayMotionAction)
         self.object_recognition_client = actionlib.SimpleActionClient('/count_objects', count_objectsAction)
         self.table_status_client = actionlib.SimpleActionClient('/tableStatus', TableStatusAction)
-        self.speech_client = actionlib.SimpleActionClient('/tts', TtsAction)
+        #self.speech_client = actionlib.SimpleActionClient('/tts', TtsAction)
 
+        # Bool variable and wake_word subscriber for voice plan activation
         self.running = False
         rospy.Subscriber('/wake_word/wake_word_detected', String, self.handle_wake_word_detected)
         self.plan_publisher = rospy.Publisher('/p1/planToExec', String, queue_size=1)
 
-        # load locations
-        config_path = rospy.get_param('~config_path', path.join(path.dirname(path.realpath(__file__)), '../../config'))
-        yaml_locations_path = path.join(config_path, 'locations.yaml')
-        yaml_locations = yaml.load(file(yaml_locations_path, 'r'))['locations']
-
-        self.locations_order = [loc['id'] for loc in yaml_locations]
-        self.locations = {
-            loc['id']: (
-                Pose(
-                    position=Point(**loc['far_pose']['position']),
-                    orientation=Quaternion(**loc['far_pose']['orientation'])
-                ), 
-                Pose(
-                    position=Point(**loc['close_pose']['position']),
-                    orientation=Quaternion(**loc['close_pose']['orientation'])
-                )
-            )
-            for loc in yaml_locations
-        }
-
+        # Get the Tables Dictionary from the parameter server
+        self.tables = rospy.get_param("/tables")
+        self.tables_order = [self.tables[table]['id'] for table in self.tables if self.tables[table]['count']]
+    
     def handle_wake_word_detected(self, data):
         wake_word = data.data
         if not self.running and wake_word == 'start the demo':
@@ -86,7 +71,7 @@ class P1Server(object):
         rospy.loginfo("----------ExternalServer end----------")
 
     def initialise(self):
-        initialise variable
+        # initialise PNP variables
         self._result.set_variables = [lpb_msg.VariableValue('tableIndex', '0'), lpb_msg.VariableValue('poseIndex', '0')]
 
     def gotoHome(self):
@@ -96,7 +81,8 @@ class P1Server(object):
 
         goal = MoveBaseGoal()
         goal.target_pose.header = Header(frame_id="map", stamp=rospy.Time.now())
-        goal.target_pose.pose = self.locations['home']
+        goal.target_pose.pose = Pose(position = Point(**self.tables['Home']['loc']['position']),
+            orientation = Quaternion(**self.tables['Home']['loc']['orientation']))
 
         rospy.loginfo('Sending goal location ...')
         self.move_base_client.send_goal(goal) #waits forever
@@ -107,13 +93,20 @@ class P1Server(object):
 
     def goto(self, table_index, pose_index):
         # TODO: move to individual action file
-        rospy.loginfo('Going to: %s' % self.locations_order[int(table_index)])
+        rospy.loginfo('Going to: %s' % self.tables_order[int(table_index)])
 
         self.move_base_client.wait_for_server(rospy.Duration(15.0))
 
+        # For now
+        if int(pose_index) == 0:
+            pose = 'far_pose'
+        else:
+            pose = 'close_pose'
+
         goal = MoveBaseGoal()
         goal.target_pose.header = Header(frame_id="map", stamp=rospy.Time.now())
-        goal.target_pose.pose = self.locations[self.locations_order[int(table_index)]][int(pose_index)]
+        goal.target_pose.pose = Pose(position = Point(**self.tables['table' + table_index]['loc'][int(pose_index)][pose]['position']),
+            orientation = Quaternion(**self.tables['table' + table_index]['loc'][int(pose_index)][pose]['orientation']))
 
         rospy.loginfo('Sending goal location ...')
         self.move_base_client.send_goal(goal) #waits forever
@@ -147,6 +140,11 @@ class P1Server(object):
         self.countPeople_result = self.object_recognition_client.get_result()
         print('{0} persons'.format(self.countPeople_result.person))
 
+        # Update the number of people in the parameter server
+        rospy.loginfo('Updating the number of people found at table %s' % self.tables_order[int(table_index)])
+        rospy.set_param('/tables/table' + table_index + '/person_count', self.countPeople_result.person)
+        rospy.loginfo('Updated the person counter successfully')
+
         # Switch to the pose closer to the table
         rospy.loginfo('Switching to the second pose')
         self._result.set_variables = [lpb_msg.VariableValue('poseIndex', str(1))]
@@ -154,7 +152,7 @@ class P1Server(object):
     # Sleeps are required to avoid Tiago's busy status from body motions controllers
     def identifyStatus(self, table_index):
         # TODO: move to individual action file
-        rospy.loginfo('Identifying the status of: %s' % self.locations_order[int(table_index)])
+        rospy.loginfo('Identifying the status of: %s' % self.tables_order[int(table_index)])
 
         # Step 1: Look down to see the table
         # Wait for the play motion server to come up and send goal
@@ -198,7 +196,7 @@ class P1Server(object):
 
         # Print the table status 
         status_result = self.table_status_client.get_result()
-        rospy.loginfo('PclCheck result of %s is %s' % (self.locations_order[int(table_index)], status_result.status))
+        rospy.loginfo('PclCheck result of %s is %s' % (self.tables_order[int(table_index)], status_result.status))
 
         # Step 4: Get head and torso back to default
         pose_goal.motion_name = "back_to_default"
@@ -206,10 +204,10 @@ class P1Server(object):
         rospy.loginfo('Default head position goal sent')
         rospy.sleep(3)
 
-        # Step 4: Decide on table status and publish it to the sound server
+        # Step 4: Decide on table status and send tts goal to the sound server
         # Create a tts goal
-        tts_goal = TtsGoal()
-        tts_goal.rawtext.lang_id = 'en_GB'
+        #tts_goal = TtsGoal()
+        #tts_goal.rawtext.lang_id = 'en_GB'
         foundPerson = False
         foundConsumable = False
         clean = False
@@ -224,25 +222,30 @@ class P1Server(object):
             clean = True
 
         if foundPerson and foundConsumable:
-            result = 'Status of {0} is Already served'.format(self.locations_order[int(table_index)])
+            result = 'Status of {0} is Already served'.format(self.tables_order[int(table_index)])
         elif foundPerson and (not foundConsumable):
-            result = 'Status of {0} is Needs serving'.format(self.locations_order[int(table_index)])
+            result = 'Status of {0} is Needs serving'.format(self.tables_order[int(table_index)])
         elif not foundPerson:
             if clean:
-                result = 'Status of {0} is Clean'.format(self.locations_order[int(table_index)])
+                result = 'Status of {0} is Clean'.format(self.tables_order[int(table_index)])
             else:
-                result = 'Status of {0} is Dirty'.format(self.locations_order[int(table_index)])
+                result = 'Status of {0} is Dirty'.format(self.tables_order[int(table_index)])
         
+        # Update the status of the table in the parameter server
+        rospy.loginfo('Updating the table status of table %s' % self.tables_order[int(table_index)])
+        rospy.set_param('/tables/table' + table_index + '/status', result)
+        rospy.loginfo('Updated the table status successfully')
+
         rospy.loginfo(result)
-        tts_goal.rawtext.text = result
-        self.tts_pub.publish(tts_goal)
+        #tts_goal.rawtext.text = result
+        #self.tts_pub.publish(tts_goal)
         rospy.sleep(1)
 
     def count(self, table_index):
         # TODO: move to individual action file
-        rospy.loginfo('Counting: %s' % self.locations_order[int(table_index)])
+        rospy.loginfo('Counting: %s' % self.tables_order[int(table_index)])
 
-        if int(table_index) == len(self.locations_order) - 1:
+        if int(table_index) == len(self.tables_order) - 1:
             self._result.condition_event = ['doneCounting']
         else:
             self._result.set_variables = [lpb_msg.VariableValue('tableIndex', str(int(table_index) + 1))]
