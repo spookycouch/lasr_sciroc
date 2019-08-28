b5801b79e09e3e14116096ca3d247db32c747884
Mohammed Alshammasi <fy16m3aa@leeds.ac.uk>
2019-08-28 03:19:38 +0000

    Updated setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-  packages=['lasr_sciroc_p1'],
+  packages=['lasr_sciroc'],
   package_dir={'': 'src'}
 )
 
