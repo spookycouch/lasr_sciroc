3e14eb06ad31690d72f203da86359c95c3cb3260
Mohammed Alshammasi <fy16m3aa@leeds.ac.uk>
2019-08-28 01:30:21 +0000

    All bug fixes, running in simulation

--- src/lasr_sciroc/p2_server.py
+++ src/lasr_sciroc/p2_server.py
@@ -11,7 +11,7 @@ import rosparam
 import lasr_pnp_bridge.msg as lpb_msg
 from std_msgs.msg import String, Header
 from actionlib_msgs.msg import GoalStatus
-from geometry_msgs.msg import Point, Quaternion, Pose
+from geometry_msgs.msg import Point, Quaternion, Pose, PoseWithCovarianceStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from lasr_object_detection_yolo.msg import yolo_detectionGoal, yolo_detectionAction
@@ -31,7 +31,7 @@ class P2Server(object):
         # Initialising clients: move_base, playmotion, objectRecognition and table_status
         self.move_base_client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
         self.play_motion_client = actionlib.SimpleActionClient('/play_motion', PlayMotionAction)
-        self.object_recognition_client = actionlib.SimpleActionClient('/count_objects', count_objectsAction)
+        self.object_recognition_client = actionlib.SimpleActionClient('/yolo_detection', yolo_detectionAction)
         self.speech_client = actionlib.SimpleActionClient('/tts', TtsAction)
 
         # Bool variable and wake_word subscriber for voice plan activation
@@ -62,80 +62,11 @@ class P2Server(object):
 
         # end callback
         rospy.loginfo("----------ExternalServer end----------")
-
-    def gotoHome(self):
-        rospy.loginfo('Going to home')
-        home = rospy.get_param('/Home')
-
-        self.move_base_client.wait_for_server(rospy.Duration(15.0))
-
-        goal = MoveBaseGoal()
-        goal.target_pose.header = Header(frame_id="map", stamp=rospy.Time.now())
-        goal.target_pose.pose = Pose(position = Point(**home['loc']['position']),
-            orientation = Quaternion(**home['loc']['orientation']))
-
-        rospy.loginfo('Sending goal location ...')
-        self.move_base_client.send_goal(goal) #waits forever
-        if self.move_base_client.wait_for_result():
-            rospy.loginfo('Goal location achieved!')
-        else:
-            rospy.logwarn("Couldn't reach the goal!")
-
-    def goto(self):
-        table_index = rospy.get_param('/HAL9000/current_table')
-        pose_index = rospy.get_param('/HAL9000/current_pose')
-        print table_index
-        # TODO: move to individual action file
-        rospy.loginfo('Going to: %d with pose %d', table_index, pose_index)
-
-        self.move_base_client.wait_for_server(rospy.Duration(15.0))
-
-        # For now
-        if pose_index == 0:
-            pose = 'far_pose'
-        else:
-            pose = 'close_pose'
-
-        goal = MoveBaseGoal()
-        goal.target_pose.header = Header(frame_id="map", stamp=rospy.Time.now())
-        goal.target_pose.pose = Pose(position = Point(**self.tables['table' + str(table_index)]['loc'][pose_index][pose]['position']),
-            orientation = Quaternion(**self.tables['table' + str(table_index)]['loc'][pose_index][pose]['orientation']))
-
-        rospy.loginfo('Sending goal location ...')
-        self.move_base_client.send_goal(goal) #waits forever
-        if self.move_base_client.wait_for_result():
-            rospy.loginfo('Goal location achieved!')
-        else:
-            rospy.logwarn("Couldn't reach the goal!")
-
-        # If we moved close to the table means we have already taken the picture
-        if pose_index == 1:
-           self._result.condition_event = ['pictureDone']
-           rospy.set_param('/HAL9000/current_pose', 0)
-
-    def count(self):
-        # TODO: move to individual action file
-        rospy.loginfo('Counting all the tables')
-
-        # if any table status is unknown, set it to the robot's current table
-        self.tables = rospy.get_param("/tables")
-        unknown_exist = False
-        for table in self.tables:
-            if (self.tables[table])['status'] == 'unknown':
-                if not unknown_exist:
-                    unknown_exist = True
-                    next_table = self.tables[table]['id']
-                elif self.tables[table]['id'] < next_table:
-                    next_table = self.tables[table]['id']
-
-        if unknown_exist:
-            rospy.set_param('/HAL9000/current_table', next_table)
-            print "\033[1;33m" + "The next table is " + str(next_table) + "\033[0m"
-        else:
-            # if all tables have been identified, counting is done
-            self._result.condition_event = ['doneCounting']
     
     def p2Initialise(self):
+        # Get the Tables Dictionary from the parameter server
+        self.tables = rospy.get_param("/tables")
+
         # Get the ID of a table that needs serving
         needServing_exist = False
         for table in self.tables:
@@ -254,7 +185,7 @@ class P2Server(object):
         pose_goal.skip_planning = True
         self.play_motion_client.send_goal(pose_goal)
         rospy.loginfo('Looking down goal sent')
-        rospy.sleep(3)
+        rospy.sleep(5)
 
         # Run the object detection client on the items
         goal = yolo_detectionGoal
@@ -264,43 +195,76 @@ class P2Server(object):
         goal.nms = 0.5
         self.object_recognition_client.send_goal(goal)
         rospy.loginfo('Object check goal sent')
-        client.wait_for_result()
-        result = client.get_result()
+        self.object_recognition_client.wait_for_result()
+        result = self.object_recognition_client.get_result()
         rospy.loginfo('Got the result back')
 
         order_count = defaultdict(int)
         for item in order:
             order_count[item] += 1
         
-        # Say what we saw
         object_count = defaultdict(int)
         for detection in result.detected_objects:
             object_count[detection.name] += 1
         for count in object_count:
             print('I see ' + str(object_count[count]) + ' of ' + str(count))
 
-        # Compare the result to the order and announce the missing item
-        for item in object_count:
-            if object_count[item] != order_count[item]:
-                missing_item = item
-                break
-        if missing_item:
-            rospy.loginfo('The missing item is %s', missing_item)
+        # Look back up
+        # Wait for the play motion server to come up and send goal
+        self.play_motion_client.wait_for_server(rospy.Duration(15.0))
+        pose_goal = PlayMotionGoal()
+        pose_goal.motion_name = "back_to_default"
+        pose_goal.skip_planning = True
+        self.play_motion_client.send_goal(pose_goal)
+        rospy.loginfo('Back to default goal sent')
+        rospy.sleep(5)
 
-        # Say the missing item
+        # Compare the result to the order and announce the missing item
+        missing_items = defaultdict(int)
+        excess_items = defaultdict(int)
+        for item in order_count:
+            if object_count[item] < order_count[item]:
+                missing_items[item] = order_count[item] - object_count[item]
+            elif object_count[item] > order_count[item]:
+                excess_items[item] = object_count[item] - order_count[item]
+
+        # Output incorrect items
+        if len(missing_items) or len(excess_items):
+            self.talk('The order is incorrect. Please correct the order and place the items on my back.') 
+            if len(missing_items):
+                self.talk('The missing items are:')
+                for item in missing_items:
+                    speech_out = str(missing_items[item]) + ' ' + str(item)
+                    if not missing_items[item] == 1:
+                        speech_out += 's'
+                    self.talk(speech_out)
+            # output excess items
+            if len(excess_items):
+                self.talk('The excess items are:')
+                for item in excess_items:
+                    speech_out = str(excess_items[item]) + ' ' + str(item)
+                    if not excess_items[item] == 1:
+                        speech_out += 's'
+                    self.talk(speech_out)
+        rospy.sleep(4)
+
+    def talk(self, speech_in):
+        print('\033[1;36mTIAGO: ' + speech_in + '\033[0m')
         tts_goal = TtsGoal()
         tts_goal.rawtext.lang_id = 'en_GB'
-        tts_goal.rawtext.text = 'The item {0} is wrong. Please correct the order and place it on my back'.format(missing_item)
+        tts_goal.rawtext.text = speech_in
         self.speech_client.send_goal(tts_goal)
-        rospy.sleep(4)
 
     
     def waitConfirmation(self):
+        rospy.sleep(2)
+        table_index = rospy.get_param('/HAL9000/current_table')
+
         # Get his current pose
-        current_pose = rospy.wait_for_message('/amcl_pose', Pose)
+        current_pose = rospy.wait_for_message('/amcl_pose', PoseWithCovarianceStamped)
 
         # Change orientation to turn TIAGo 180 degrees
-        current_pose.orientation.w = -current_pose.orientation.w
+        current_pose.pose.pose.orientation.w = -current_pose.pose.pose.orientation.w
         turn  = current_pose
 
         # Send new pose
@@ -308,7 +272,7 @@ class P2Server(object):
 
         goal = MoveBaseGoal()
         goal.target_pose.header = Header(frame_id="map", stamp=rospy.Time.now())
-        goal.target_pose.pose = turn
+        goal.target_pose.pose = turn.pose.pose
         rospy.loginfo('Sending goal location ...')
         self.move_base_client.send_goal(goal) #waits forever
         if self.move_base_client.wait_for_result():
@@ -319,3 +283,6 @@ class P2Server(object):
         # Wait for keyword detection porcupine
         # nothing for now
         rospy.sleep(15)
+
+        # Set the table to be already served
+        rospy.set_param('/tables/table' + str(table_index) + '/status', 'Already served')
