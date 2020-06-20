38bb1b8a2a0dfdab240d6652254f132a7074f70f
joejeffcock <sc18j3j@leeds.ac.uk>
2020-06-20 01:08:44 +0100

    chore: tested dialogflow, it works lol

--- src/lasr_sciroc/main.py
+++ src/lasr_sciroc/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import rospy
+import actionlib
 
 from SciRocServer import SciRocServer
 from p1_server import P1Server
 from p2_server import P2Server
 from p3_server import P3Server
+
+from lasr_dialogflow.msg import DialogAction, DialogGoal
 
 if __name__ == '__main__':
     # init the node so we can access stuff
@@ -12,6 +15,9 @@ if __name__ == '__main__':
     p1 = P1Server('server1')
     p2 = P2Server('server2')
     p3 = P3Server('server3')
+
+    dialog_client = actionlib.SimpleActionClient('/lasr_dialogflow/LasrDialogflow', DialogAction)
+    dialog_client.wait_for_server()
 
     # --------------- #
     # P2 SERVER START #
@@ -27,6 +33,9 @@ if __name__ == '__main__':
         print 'done serving'
     else:
         print 'not done serving'
+
+    # use dialogflow to take the customer's order
+    # dialog_client.send_goal(DialogGoal('takeOrder'))
     
     # go to the unserved table
     p2.gotoTable('0')
